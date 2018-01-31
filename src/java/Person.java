2daa5af9a5baf8ca778b03647ff948d3e1629525
Marek Dudek <mdudek15@gmail.com>
2018-01-31 16:10:51 +0000

    Test string added
    Test string added

--- src/java/Person.java
+++ src/java/Person.java
@@ -23,6 +23,7 @@ public class Person {
     String selectedStream;
     String selectedLocation;
     int assessmentYear;
+    String test;
     public Person(String firstName, String surname, String homeAddress, int contactNumber, String email, String degree, int yearIn, int yearOut, String interest, String availability, String selectedStream, String selectedLocation, int assessmentYear){
     this.id=1;
      this.firstName=firstName;
