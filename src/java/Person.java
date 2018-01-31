0b38cbbec578762b26951410a8dbdc6f132e960b
Marek Dudek <mdudek15@gmail.com>
2018-01-31 15:47:31 +0000

    Person
    Person class added

---
+++ src/java/Person.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Marek
+ */
+public class Person {
+    int id;
+    String firstName;
+    String surname;
+    String homeAddress;
+    int contactNumber;
+    String email;
+    String degree;
+    int yearIn;
+    int yearOut;
+    String interest;
+    String availability;
+    String selectedStream;
+    String selectedLocation;
+    int assessmentYear;
+    public Person(String firstName, String surname, String homeAddress, int contactNumber, String email, String degree, int yearIn, int yearOut, String interest, String availability, String selectedStream, String selectedLocation, int assessmentYear){
+    this.id=1;
+     this.firstName=firstName;
+     this.surname=surname;
+     this.homeAddress=homeAddress;
+     this.contactNumber=contactNumber;
+     this.email=email;
+     this.degree=degree;
+     this.yearIn=yearIn;
+     this.yearOut=yearOut;
+     this.interest=interest;
+     this.availability=availability;
+     this.selectedStream=selectedStream;
+     this.selectedLocation=selectedLocation;
+     this.assessmentYear=assessmentYear;
+    }
+    
+}
