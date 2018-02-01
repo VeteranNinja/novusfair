fc76e631cfa2c7d5c831a0022525e71d930daea4
[Marek] <mdudek15@gmail.com>
2018-02-01 16:41:16 +0000

    AddPerson() should be funcational now

--- src/main/java/com/novus/novusfair/LogicLayer.java
+++ src/main/java/com/novus/novusfair/LogicLayer.java
@@ -18,7 +18,7 @@ import org.hibernate.cfg.Configuration;
  */
 public class LogicLayer {
     
-    public static void main(String [] args)
+    public static void main(String [] args) // FOR TESTING ONLY
     {
         Person person = new Person("John", "Snow", "9 Walker Road", "00000009001", "superjohnsnow@gmail.com", "Computer Science", 2012, 2015, "Making potato salads", "2018-02-22", "SoftwareDevelopment", "Manchester", 2018);
         Configuration con = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Person.class);
@@ -29,7 +29,13 @@ public class LogicLayer {
         tx.commit();
     }
     public void addPerson(String firstName, String surname, String homeAddress, String contactNumber, String email, String degree, int yearIn, int yearOut, String interest, String availability, String selectedStream, String selectedLocation, int assessmentYear){
-        
+        Person person = new Person(firstName, surname, homeAddress, contactNumber, email, degree, yearIn, yearOut, interest, availability, selectedStream, selectedLocation, assessmentYear);
+        Configuration con = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Person.class);
+        SessionFactory sf = con.buildSessionFactory();
+        Session session = sf.openSession();
+        Transaction tx = session.beginTransaction();
+        session.save(person);
+        tx.commit();
     }
     public void deletePerson(int id){
         
